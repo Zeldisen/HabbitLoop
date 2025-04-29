d407229dc3c25946bfb690ea4799f4d277a78449
Zeldisen <jeanette.norberg82@gmail.com>
2025-04-29 10:48:56 +0200

    Initial Commit

---
+++ HabbitLoop/HabbitLoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabbitLoopApp.swift
+//  HabbitLoop
+//
+//  Created by Jeanette Norberg on 2025-04-29.
+//
+
+import SwiftUI
+
+@main
+struct HabbitLoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HabbitLoopTests/HabbitLoopTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabbitLoopTests.swift
+//  HabbitLoopTests
+//
+//  Created by Jeanette Norberg on 2025-04-29.
+//
+
+import Testing
+@testable import HabbitLoop
+
+struct HabbitLoopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
